b5b1b21c29f4086413607460ec54766abfdc743a
Roman <youngkicker123@gmail.com>
2022-06-05 10:28:14 +0300

    models

---
+++ Messenger/Models/Message.cs
@@ -0,0 +1,13 @@
+﻿namespace Messenger.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string? Text { get; set; }
+        public DateTime SendDate { get; set; }
+        public int UserId { get; set; }
+        public User? User { get; set; }
+        public int DialogId { get; set; }
+        public Dialog? Dialog { get; set; }
+    }
+}

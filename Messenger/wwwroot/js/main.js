5765e1072b0e1a95b074ae1fec4626f30490527b
FDevsoc <Fs0c@tutanota.com>
2022-06-16 14:28:09 +0300

    Add realtime data exchange

--- Messenger/wwwroot/js/main.js
+++ Messenger/wwwroot/js/main.js
@@ -1,149 +1,148 @@
+// Данные пользователя
 var currentUser;
-var Messages = [];
+var messages = [];
 var userFriends = [];
 
-//Элементы окн(диалогов,чата и текст-арии)
-var friend = document.getElementById('friendMenu');
-var dialog_win = document.getElementById('DialogWin');
-var textMess = document.getElementById('usertext');
+// Название карточки (никнейм собеседника)
+var cardDialogName;
+// Карточка диалога (HTML элемент)
+var cardDialog;
 
-//Контейнер для сообщений
-var dialogContainer;
+// Элементы окн (диалогов, чата и поля ввода)
+var friendMenu = document.getElementById('friendMenu');
+var dialogWindow = document.getElementById('DialogWin');
+var messageInputField = document.getElementById('usertext');
 
-/*userName = document.getElementById("username");*/
+// Контейнер для карточек сообщений 
+var messageCardsContainer;
+
+// Хранилище карточек диалогов
+var dialogCardsContainer = document.getElementsByClassName('userCard');
 
 const hubConnection = new signalR.HubConnectionBuilder()
     .withUrl("MainChat")
     .build();
 
-// получение сообщения от сервера
-hubConnection.on('GetData', function (currentClient, messages, friendList) {
+// Получение сообщения от сервера
+hubConnection.on('GetData', function (currentClient, messageList, friendList) {
     currentUser = currentClient;
-    Messages = messages;
+    messages = messageList;
     userFriends = friendList;
+
+    showDialogWindow(cardDialogName);
 });
 
-// отправка сообщения на сервер
+// Отправка сообщения на сервер
 function GetUserData() {
     let userName = document.getElementById("username");
-    setTimeout(function () { hubConnection.invoke("GetData", userName.innerHTML) }, 300);
-}
-
-function SendData(message) {
-    setTimeout(function () { hubConnection.invoke("SendData", currentUser, message); }, 300);
+    hubConnection.invoke("GetData", userName.innerHTML);
 }
 
 hubConnection.start();
 
-GetUserData();
 
-//метод выполняемый при загрузки всего dom'а и ресурсов
-Start();
+if (dialogCardsContainer) {
+    // Перебор карточек и добавление каждой прослушки
+    Array.from(dialogCardsContainer).forEach(cardDialogItem => {
+        cardDialogItem.addEventListener('click', function () {
+            cardDialogName = cardDialogItem.firstElementChild.firstElementChild.innerText;
+            cardDialog = cardDialogItem;
 
-//метод добавления диалогам  прослушивания событий
-function Start() {
-    //массив карточек
-    var user_card = document.getElementsByClassName('userCard');
-    if (user_card) {
-        //перебор карточек и добавление каждой прослушки
-        Array.from(user_card).forEach(u_card => {
-            u_card.addEventListener('click', function () {
-                //удаление заглушки "нет сообщений"
-                var sub = document.getElementById('subtextWin');
-                sub.classList.add("d-none");
-                //для мобилки убираем отображение диалогов и добавляем отображение окна сообщений
-                dialog_win.classList.remove("d-none");
-                friend.classList.add("d-none");
-                //добавляем кнопке, а именно стиль "активной"
-                var ch = u_card.firstElementChild;
-                u_card.classList.add('border-primary');
-                ch.classList.add("bushcard");
-                //удаляем стиль "активной" у остальных других, если такие имеются
-                for (var i = 0; user_card.length > i; i++) {
-                    var fItem = user_card[i].classList;
-                    if (fItem.contains("border-primary") & u_card.classList != fItem) {
-                        fItem.remove("border-primary");
-                    }
-                    var item = user_card[i].firstElementChild.classList;
-                    if (item.contains("bushcard") & u_card.classList != fItem) {
-                        item.remove("bushcard");
-                    }
+            GetUserData();
+
+            var dialogWindowPlaceholder = document.getElementById('subtextWin');
+            dialogWindowPlaceholder.classList.add("d-none");
+
+            // Убирает отображение диалогов и добавляет отображение окна сообщений на мобильных устройствах
+            dialogWindow.classList.remove("d-none");
+            friendMenu.classList.add("d-none")
+
+            var mainDialogWindow = document.getElementById('chatWin');
+            mainDialogWindow.classList.remove("d-none");
+
+            var dialogCardBody = cardDialogItem.firstElementChild;
+            cardDialogItem.classList.add('border-primary');
+            dialogCardBody.classList.add("bushcard");
+
+            // Удаляет стиль "активной" у остальных других, если такие имеются
+            for (var i = 0; dialogCardsContainer.length > i; i++) {
+                var cardClassList = dialogCardsContainer[i].classList;
+                if (cardClassList.contains("border-primary") &&
+                    cardDialogItem.classList != cardClassList) {
+                    cardClassList.remove("border-primary");
                 }
-                //Отображем окно сообщения(для мобилки)
-                ShowMessangesWin(ch.firstElementChild.innerText);
-            })
-        })
-    }
+
+                var cardBodyClassList = dialogCardsContainer[i].firstElementChild.classList;
+                if (cardBodyClassList.contains("bushcard") & cardDialogItem.classList != cardClassList) {
+                    cardBodyClassList.remove("bushcard");
+                }
+            }
+        });
+    });
 }
 
 
-//метод просто отображения окна сообщений
-function ShowMessangesWin(Username) {
-    var chatbox = document.getElementById('chatWin');
-    chatbox.classList.remove("d-none");
-    document.getElementById('dialog-info').innerText = Username;
-
-    UpdateMess(Username);
+// Метод отображения окна сообщений
+function showDialogWindow(friendName) {
+    document.getElementById('dialog-info').innerText = friendName;
+    updateMessages(friendName);
 }
 
 
-//Кнопка "назад" (для мобилки)
-var backButt = document.getElementById('backBtn');
-//обработчик события на нажатие кнопки "назад"
-backButt.onclick = function () {
-    dialog_win.classList.add("d-none");
-    friend.classList.remove("d-none");
+// Кнопка "назад" (для мобильных устройств)
+var backBtn = document.getElementById('backBtn');
+
+// Обработчик события клика на кнопку "назад"
+backBtn.onclick = () => {
+    dialogWindow.classList.add("d-none");
+    friendMenu.classList.remove("d-none");
 }
 
 
-//Кнопка "Отправить сообщение"
+// Кнопка отправки сообщения
 var sendBtn = document.getElementById('sendMessange');
-//Обработчик кнопки "Отправить сообщение"
-sendBtn.addEventListener('click', SendMessange);
+// Событие клика на кнопку отправки сообщения
+sendBtn.addEventListener('click', sendMessange);
 
 
-//функция выполняемая при нажатие на кнопку "Отправить сообщение"
-function SendMessange() {
-    //Хранилище для сообщения которое набрали
-    var message = {};
-    //id пользователя которому отправляется сообщение
-    var recId = null;
-    Array.from(userFriends).forEach(userFr => {
-        if (userFr.name === document.getElementById('dialog-info').innerHTML) {
-            recId = userFr.id;
-            reciverName = userFr.name;
+// Обработчик события отправки сообщения
+function sendMessange() {
+    // Объект нового сообщения
+    let message;
+    // Id и имя пользователя которому отправляется сообщение
+    let receiverId;
+    let reciverName;
+
+    Array.from(userFriends).forEach(userFriend => {
+        if (userFriend.name === document.getElementById('dialog-info').innerHTML) {
+            receiverId = userFriend.id;
+            reciverName = userFriend.name;
         }
     })
-    //name пользователя которому отправляется сообщение
-    var reciverName;
-    //Проверка на то что сообщение кому то вообще адресовано(переписать под ваш лад)
-    if (recId != null) {
-        //Получаем текущее время в формате(вся инфа)
-        var nowdate = new Date();
-        //Получаем из этой инфы только текущие часы и минуты
-        var hrs = nowdate.getHours();
-        var min = nowdate.getMinutes();
 
-        if (hrs < 10) hrs = "0" + hrs;
-        if (min < 10) min = "0" + min;
-        //Записываем их в строку
-        let timesend = hrs + ":" + min;
-        //Проверка на то что сообщение не пустое
-        if (textMess.value) {
-            //формирование сообщения
-            message = { Id: 0, Text: textMess.value, SendDate: nowdate, SenderId: currentUser.id, ReceiverId: recId, DialogId: 0 };
-            SendData(message);
-        }
+    if (messageInputField.value) {
+        // Формирование объекта сообщения
+        message = {
+            Id: 0,
+            Text: messageInputField.value,
+            SendDate: new Date(),
+            SenderId: currentUser.id,
+            ReceiverId: receiverId,
+            DialogId: 0
+        };
+
+        hubConnection.invoke("SendData", currentUser, message);
     }
 
     // Очистка поля ввода сообщения после нажатия на кнопку
-    textMess.value = "";
-
-    UpdateMess(reciverName);
+    messageInputField.value = "";
+    updateMessages(reciverName);
 }
-//Функция создания карточек сообщений(разные стили)
-function CreateMessage(Message) {
-    if (currentUser.id === Message.senderId) {
+
+
+//Функция создания карточек сообщений 
+function createMessage(message) {
+    if (currentUser.id === message.senderId) {
         let card = document.createElement('div');
         card.className = 'card mb-3 text-white senderBox bg-primary ';
 
@@ -151,21 +150,23 @@ function CreateMessage(Message) {
         cardBody.className = 'card-body';
 
         let text = document.createElement('div');
-        text.innerText = Message.text;
+        text.innerText = message.text;
         text.className = 'card-text text-start ';
 
         let footer = document.createElement('div');
-        if (Message.sendDate.length > 5) {
-            Message.sendDate = Message.sendDate.substring(11, 16);
-        }
-        footer.innerText = Message.sendDate;
-        footer.className = 'label text-end labelData small ';
 
+        // Формат даты
+        if (message.sendDate.length > 5) {
+            message.sendDate = message.sendDate.substring(11, 16);
+        }
+
+        footer.innerText = message.sendDate;
+        footer.className = 'label text-end labelData small ';
 
         cardBody.appendChild(text);
         cardBody.appendChild(footer);
         card.appendChild(cardBody);
-        dialogContainer.appendChild(card);
+        messageCardsContainer.appendChild(card);
     } else {
         let card = document.createElement('div');
         card.className = 'card mb-3 text-white bg-dark reciverBox ';
@@ -174,57 +175,56 @@ function CreateMessage(Message) {
         cardBody.className = 'card-body';
 
         let text = document.createElement('div');
-        text.innerText = Message.text;
+        text.innerText = message.text;
         text.className = 'card-text text-start';
 
+        if (message.sendDate.length > 5) message.sendDate = message.sendDate.substring(11, 16);
+
         let footer = document.createElement('div');
-        if (Message.sendDate.length > 5) {
-            Message.sendDate = Message.sendDate.substring(11, 16);
-        }
-        footer.innerText = Message.sendDate;
+        footer.innerText = message.sendDate;
         footer.className = 'label text-muted text-end labelData small';
 
         cardBody.appendChild(text);
         cardBody.appendChild(footer);
         card.appendChild(cardBody);
-        dialogContainer.appendChild(card);
+        messageCardsContainer.appendChild(card);
     }
 };
 
 
-function initMessange(Message) {
-    if (dialogContainer) {
-        document.getElementById('messengeWin').replaceWith(dialogContainer);
-    }
+function initMessages(messages) {
+    if (messageCardsContainer) document.getElementById('messengeWin').replaceWith(messageCardsContainer);
+    messageCardsContainer = document.getElementById('messengeWin');
 
-    dialogContainer = document.getElementById('messengeWin');
-    var sub = document.getElementById('subtext');
+    let dialogMenuPlaceholder = document.getElementById('subtext');
+    if (dialogMenuPlaceholder) dialogMenuPlaceholder.classList.add("d-none");
 
-    if (sub) {
-        sub.classList.add("d-none");
-    }
-
-    Array.from(Message).forEach(Mes => CreateMessage(Mes));
-
+    Array.from(messages).forEach(message => createMessage(message));
 };
-function UpdateMess(Username) {
-    var messange = document.getElementById('messengeWin');
-    while (messange.firstChild) {
-        messange.removeChild(messange.firstChild);
+
+
+function updateMessages(friendName) {
+    GetUserData();
+    updateMessageCards(friendName);
+};
+
+
+function updateMessageCards(friendName) {
+    let messageWindow = document.getElementById('messengeWin');
+    while (messageWindow.firstChild) {
+        messageWindow.removeChild(messageWindow.firstChild);
     }
-    var thisFriendMessanges = [];
-    Array.from(Messages).forEach(Mess => {
-        for (var i = 0; userFriends.length > i; i++) {
-            if ((userFriends[i].name === Username) && 
-                (userFriends[i].id === Mess.receiverId || userFriends[i].id === Mess.senderId) &&
-                (currentUser.id === Mess.receiverId || currentUser.id === Mess.senderId))
-            {
-                thisFriendMessanges.push(Mess);
+
+    let messageList = [];
+    Array.from(messages).forEach(message => {
+        for (let i = 0; userFriends.length > i; i++) {
+            if ((userFriends[i].name === friendName) &&
+                (userFriends[i].id === message.receiverId || userFriends[i].id === message.senderId) &&
+                (currentUser.id === message.receiverId || currentUser.id === message.senderId)) {
+                messageList.push(message);
             }
         }
     })
-    initMessange(thisFriendMessanges);
 
-    // Задает скролл листа сообщений таким образом что при добавлении скрол сам будет опускаться вниз
-    document.getElementById('messengeWin').scrollIntoView(false);
-};
+    initMessages(messageList);
+}
